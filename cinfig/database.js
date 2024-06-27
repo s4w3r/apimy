90641106618b411405a3680ba8093d5334879dca
paula <juliaan.roy@gmail.com>
2024-06-27 12:59:36 -0500

    Comiteho

---
+++ cinfig/database.js
@@ -0,0 +1,9 @@
+const dab = {
+    host: "monorail.proxy.rlwy.net",
+    user: "root",
+    password: 'PuqkCmKYNbrNVEItFgujEhJksZhPfpLb',
+    database: 'railway',
+    port: 49736,
+}
+
+export default dab
